a29075f5e081b66e6c69f7707268aa824a0d0547
LoshakovaAnna <ann.loshakova.111@gmail.com>
2018-10-03 16:54:44 +0300

    add webpack files

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const webpack = require('webpack');
+const path = require('path');
+module.exports={
+    entry:'./app.js',
+    output:{
+        filename:'bundle.js',
+    },
+    devServer:{
+        index:'index.html',
+    },
+    watch:true,
+  module: {
+    
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader",
+        options: {
+          includePaths: []
+        }
+      }]
+      }, {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      }, {
+        test: /\.(gif|svg|jpg|png)$/,
+        loader: "file-loader",
+      }]
+    },
+
+  plugins: [
+    new LiveReloadPlugin({}),
+    new webpack.WatchIgnorePlugin([
+      path.join(__dirname, "node_modules")
+    ]),
+  ]
+}
